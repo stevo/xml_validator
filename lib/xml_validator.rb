77fde445a0cef078cd798c9026d2e932e1c1bb17
Blazej Kosmowski <b.kosmowski@selleo.com>
2009-10-11 17:22:28 +0200

    Documentation and refactor

--- lib/xml_validator.rb
+++ lib/xml_validator.rb
@@ -2,37 +2,34 @@ require 'libxml'
 
 module XmlValidator
 
-  # this method validate if given xml file (xml_filename) is valid to XSD schema
-  # parma xsd can be file including correct XSD schema or a string
-  # for example:
-  # XmlValidator.validate("C:\my_file.xml", "C:\my_file.xsd")
-  # XmlValidator.validate("C:\my_file.xml", string_schema) where string_schema is a string including correct XSD schema
-  def self.validate_file(xml_filename, xsd)
+  # Tis method validates if given xml file (xml_filename) is valid against XSD schema
+  # As param you can supply correct XSD schema or a string representing this secham
+ 
+  def self.validate_abstract(xsd, &block)
     begin
       messages = []
-      document = LibXML::XML::Document.file(xml_filename)
-      schema = File.exist?("#{xsd}") ? LibXML::XML::Schema.new(xsd) : XML::Schema.from_string(xsd)
+      document = block.call
+      schema = File.exist?("#{xsd}") ? ::LibXML::XML::Schema.new(xsd) : ::XML::Schema.from_string(xsd)
       result = document.validate_schema(schema) do |message|
         messages << message
       end
     rescue Exception => e
       messages << "General Error: #{e.message}"
     end
-    [result || false, messages.to_sentence]
+    [result || false, messages * ' ']
+  end
+
+
+  def self.validate_file(xml_filename, xsd)
+    validate_abstract(xsd) do
+      ::LibXML::XML::Document.file(xml_filename)
+    end
   end
 
   def self.validate_string(xml_string, xsd)
-    begin
-      messages = []
-      document = LibXML::XML::Document.string(xml_string)
-      schema = File.exist?("#{xsd}") ? LibXML::XML::Schema.new(xsd) : XML::Schema.from_string(xsd)
-      result = document.validate_schema(schema) do |message|
-        messages << message
-      end
-    rescue Exception => e
-      messages << "General Error: #{e.message}"
+    validate_abstract(xsd) do
+      ::LibXML::XML::Document.string(xml_string)
     end
-    [result || false, messages.to_sentence]
   end
 
 end
